3054ef49bb5c0b0ed7b9987d9b23024e094525d5
Salamandra <zxjoaozx@gmail.com>
2025-04-15 16:13:07 -0300

    feat: resolution for exercise 1016

---
+++ lista-1/1016/1016.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  int distancia;
+
+  cin >> distancia;
+
+  cout << distancia * 2 << " minutos" << endl;
+
+  return 0;
+}
